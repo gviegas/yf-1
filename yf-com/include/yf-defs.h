f341643f97ef8ee624ae213dab37d5e5a9eb2e08
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-05 11:37:14 -0300

    com yf-defs.h

---
+++ yf-com/include/yf-defs.h
@@ -0,0 +1,20 @@
+/*
+ * YF
+ * yf-defs.h
+ *
+ * Copyright © 2020 Gustavo C. Viegas.
+ */
+
+#ifndef YF_DEFS_H
+#define YF_DEFS_H
+
+/* Linkage macros. */
+#ifdef __cplusplus
+# define YF_DECLS_BEGIN extern "C" {
+# define YF_DECLS_END }
+#else
+# define YF_DECLS_BEGIN
+# define YF_DECLS_END
+#endif
+
+#endif /* YF_DEFS_H */
