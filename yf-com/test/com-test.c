f96a9bdbe20cf53bdf8fe84081c9752e3571eb68
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-05 15:48:47 -0300

    com test

---
+++ yf-com/test/com-test.c
@@ -0,0 +1,18 @@
+/*
+ * YF
+ * com-test.c
+ *
+ * Copyright © 2020 Gustavo C. Viegas.
+ */
+
+#include <stdio.h>
+
+#include "test.h"
+
+/* Test function. */
+static int test(int argc, char *argv[]) {
+  /* TODO */
+  puts("TODO...");
+}
+
+const YF_test yf_g_test = {"com", test};
