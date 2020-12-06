f216b0c0c0b6241945f4e6cc77caaa200b088cda
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-06 11:13:37 -0300

    com test_hashset()

--- yf-com/test/com-test.c
+++ yf-com/test/com-test.c
@@ -12,6 +12,7 @@
 #include "error.h"
 #include "clock.h"
 #include "list.h"
+#include "hashset.h"
 
 #undef YF_TEST_LN
 #define YF_TEST_LN "..............................."
@@ -179,12 +180,12 @@ static int test_list(void) {
   puts("(list end)");
 
   printf("(list beg) ");
-  yf_list_each(ls, list_cb, NULL);
+  yf_list_each(ls, list_cb, (void*)0x1f);
   puts("(list end)");
 
   yf_list_remove(ls, &c);
 
-  printf("(list beg) ");
+  printf("\n(list beg) ");
   it = YF_NILIT;
   for (;;) {
     int *v = yf_list_next(ls, &it);
@@ -200,7 +201,7 @@ static int test_list(void) {
 
   yf_list_clear(ls);
 
-  printf("(list beg) ");
+  printf("\n(list beg) ");
   it = YF_NILIT;
   for (;;) {
     int *v = yf_list_next(ls, &it);
@@ -225,13 +226,250 @@ static int test_list(void) {
   if (yf_list_contains(ls, &d))
     return -1;
 
+  yf_list_deinit(ls);
+
   puts("");
   return 0;
 }
 
 /* Hashset test. */
+static size_t hashset_hash(const void *x) {
+  if (x == NULL)
+    return 1<<24;
+  const char *s = x;
+  return ((size_t)s[0] << 16) | ((size_t)s[1] << 8) | (size_t)s[2];
+}
+static int hashset_cmp(const void *a, const void *b) {
+  return strcmp(a, b);
+}
+static int hashset_cb(void *val, void *arg) {
+  printf("%s,%p ", (const char *)val, arg);
+  if (strcmp(val, "_B_") == 0) {
+    printf("{early break} ");
+    return 1;
+  }
+  return 0;
+}
 static int test_hashset(void) {
-  /* TODO */
+  YF_TEST_SUBT;
+
+  const char a[] = "_A_";
+  const char b[] = "_B_";
+  const char c[] = "_C_";
+  const char d[] = "_D_";
+  const char d1[] = "_D_";
+
+  YF_hashset hs = yf_hashset_init(hashset_hash, hashset_cmp);
+
+  if (yf_hashset_getlen(hs) != 0)
+    return -1;
+  if (yf_hashset_contains(hs, NULL))
+    return -1;
+
+  yf_hashset_insert(hs, d);
+  if (yf_hashset_getlen(hs) != 1)
+    return -1;
+  if (!yf_hashset_contains(hs, d))
+    return -1;
+  if (!yf_hashset_contains(hs, d1))
+    return -1;
+  if (yf_hashset_insert(hs, d) == 0)
+    return -1;
+  if (yf_hashset_insert(hs, d1) == 0)
+    return -1;
+  if (yf_hashset_search(hs, d1) != d)
+    return -1;
+
+  yf_hashset_insert(hs, c);
+  if (yf_hashset_getlen(hs) != 2)
+    return -1;
+  if (!yf_hashset_contains(hs, c))
+    return -1;
+  if (!yf_hashset_contains(hs, d))
+    return -1;
+  if (yf_hashset_insert(hs, c) == 0)
+    return -1;
+  if (yf_hashset_insert(hs, d) == 0)
+    return -1;
+  if (yf_hashset_search(hs, c) != c)
+    return -1;
+
+  yf_hashset_insert(hs, b);
+  if (yf_hashset_getlen(hs) != 3)
+    return -1;
+  if (!yf_hashset_contains(hs, b))
+    return -1;
+  if (!yf_hashset_contains(hs, d1))
+    return -1;
+  if (!yf_hashset_contains(hs, c))
+    return -1;
+  if (yf_hashset_contains(hs, a))
+    return -1;
+  if (yf_hashset_insert(hs, b) == 0)
+    return -1;
+  if (yf_hashset_search(hs, "_B_") != b)
+    return -1;
+
+  YF_iter it;
+
+  printf("\n(set beg) ");
+  it = YF_NILIT;
+  for (;;) {
+    const char *s = yf_hashset_next(hs, &it);
+    if (YF_IT_ISNIL(it))
+      break;
+    printf("%s ", s);
+  }
+  puts("(set end)");
+
+  printf("(set beg) ");
+  yf_hashset_each(hs, hashset_cb, (void *)0xff);
+  puts("(set end)");
+
+  yf_hashset_remove(hs, d1);
+  if (yf_hashset_getlen(hs) != 2)
+    return -1;
+  if (yf_hashset_contains(hs, d))
+    return -1;
+  if (yf_hashset_contains(hs, d1))
+    return -1;
+  if (!yf_hashset_contains(hs, b))
+    return -1;
+  if (yf_hashset_search(hs, d) != NULL)
+    return -1;
+
+  printf("\n(set beg) ");
+  it = YF_NILIT;
+  for (;;) {
+    const char *s = yf_hashset_next(hs, &it);
+    if (YF_IT_ISNIL(it))
+      break;
+    printf("%s ", s);
+  }
+  puts("(set end)");
+
+  printf("(set beg) ");
+  yf_hashset_each(hs, hashset_cb, NULL);
+  puts("(set end)");
+
+  yf_hashset_remove(hs, c);
+  if (yf_hashset_getlen(hs) != 1)
+    return -1;
+  if (yf_hashset_contains(hs, d))
+    return -1;
+  if (yf_hashset_contains(hs, d1))
+    return -1;
+  if (!yf_hashset_contains(hs, b))
+    return -1;
+  if (yf_hashset_remove(hs, c) == 0)
+    return -1;
+  if (yf_hashset_insert(hs, "_B_") == 0)
+    return -1;
+
+  printf("\n(set beg) ");
+  it = YF_NILIT;
+  for (;;) {
+    const char *s = yf_hashset_next(hs, &it);
+    if (YF_IT_ISNIL(it))
+      break;
+    printf("%s ", s);
+  }
+  puts("(set end)");
+
+  printf("(set beg) ");
+  yf_hashset_each(hs, hashset_cb, NULL);
+  puts("(set end)");
+
+  yf_hashset_insert(hs, a);
+  if (yf_hashset_getlen(hs) != 2)
+    return -1;
+  if (!yf_hashset_contains(hs, a))
+    return -1;
+  if (!yf_hashset_contains(hs, b))
+    return -1;
+  if (yf_hashset_contains(hs, c))
+    return -1;
+  if (yf_hashset_contains(hs, "_C_"))
+    return -1;
+  if (yf_hashset_contains(hs, d))
+    return -1;
+
+  printf("\n(set beg) ");
+  it = YF_NILIT;
+  for (;;) {
+    const char *s = yf_hashset_next(hs, &it);
+    if (YF_IT_ISNIL(it))
+      break;
+    printf("%s ", s);
+  }
+  puts("(set end)");
+
+  printf("(set beg) ");
+  yf_hashset_each(hs, hashset_cb, NULL);
+  puts("(set end)");
+
+  it = YF_NILIT;
+  void *val = yf_hashset_extract(hs, &it);
+  if (yf_hashset_getlen(hs) != 1)
+    return -1;
+  if (yf_hashset_contains(hs, val))
+    return -1;
+
+  printf("\n(set beg) ");
+  it = YF_NILIT;
+  for (;;) {
+    const char *s = yf_hashset_next(hs, &it);
+    if (YF_IT_ISNIL(it))
+      break;
+    printf("%s ", s);
+  }
+  puts("(set end)");
+
+  printf("(set beg) ");
+  yf_hashset_each(hs, hashset_cb, NULL);
+  puts("(set end)");
+
+  yf_hashset_clear(hs);
+  if (yf_hashset_getlen(hs) != 0)
+    return -1;
+  if (yf_hashset_contains(hs, a))
+    return -1;
+  if (yf_hashset_contains(hs, b))
+    return -1;
+  if (yf_hashset_contains(hs, c))
+    return -1;
+  if (yf_hashset_contains(hs, d))
+    return -1;
+  if (yf_hashset_contains(hs, d1))
+    return -1;
+  if (yf_hashset_contains(hs, "_D_"))
+    return -1;
+
+  printf("\n(set beg) ");
+  it = YF_NILIT;
+  for (;;) {
+    const char *s = yf_hashset_next(hs, &it);
+    if (YF_IT_ISNIL(it))
+      break;
+    printf("%s ", s);
+  }
+  puts("(set end)");
+
+  printf("(set beg) ");
+  yf_hashset_each(hs, hashset_cb, NULL);
+  puts("(set end)");
+
+  it = YF_NILIT;
+  if (yf_hashset_extract(hs, &it) != NULL)
+    return -1;
+  if (yf_hashset_search(hs, d) != NULL)
+    return -1;
+  if (yf_hashset_getlen(hs) != 0)
+    return -1;
+
+  yf_hashset_deinit(hs);
+
+  puts("");
   return 0;
 }
 
