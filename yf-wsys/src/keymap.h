d3bc60496fa087fe36aad323643f19d55bc88945
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-09 21:06:10 -0300

    wsys keymap

---
+++ yf-wsys/src/keymap.h
@@ -0,0 +1,21 @@
+/*
+ * YF
+ * keymap.h
+ *
+ * Copyright © 2020 Gustavo C. Viegas.
+ */
+
+#ifndef YF_KEYMAP_H
+#define YF_KEYMAP_H
+
+#include <stddef.h>
+
+/* Converts from a system-specific keycode to a 'YF_KEY' value. */
+#define YF_KEY_FROM(code) \
+  ((size_t)(code) < yf_g_keymap_n ? yf_g_keymap[(code)] : YF_KEY_UNKNOWN)
+
+/* Keymap. */
+extern const int yf_g_keymap[];
+extern const size_t yf_g_keymap_n;
+
+#endif /* YF_KEYMAP_H */
