a6d8d612caf540cc3e8b205607609fed9c21c801
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-13 13:31:50 -0300

    ext new view

--- yf-ext/include/yf-view.h
+++ yf-ext/include/yf-view.h
@@ -8,6 +8,8 @@
 #ifndef YF_YF_VIEW_H
 #define YF_YF_VIEW_H
 
+#include <yf/wsys/yf-window.h>
+
 #include "yf-common.h"
 #include "yf-scene.h"
 
@@ -17,7 +19,7 @@ YF_DECLS_BEGIN
 typedef struct YF_view_o *YF_view;
 
 /* Initializes a new view. */
-YF_view yf_view_init(YF_dim2 dim, const char *name);
+YF_view yf_view_init(YF_window win);
 
 /* Sets a view's scene. */
 void yf_view_setscene(YF_view view, YF_scene scn);
@@ -26,30 +28,14 @@ void yf_view_setscene(YF_view view, YF_scene scn);
 int yf_view_render(YF_view view);
 
 /* Starts a view's rendering loop. */
-int yf_view_start(
-  YF_view view,
-  unsigned fps,
-  void (*update)(double elapsed_time));
+int yf_view_start(YF_view view, unsigned fps,
+    void (*update)(double elapsed_time));
 
 /* Stops a view's rendering loop. */
 void yf_view_stop(YF_view view);
 
 /* Deinitializes a view. */
 void yf_view_deinit(YF_view view);
-
-/* Installs a global callback to receive pointer button events. */
-void yf_view_onbutton(void (*fn)(int btn, int state, void *data), void *data);
-
-/* Installs a global callback to receive pointer motion events. */
-void yf_view_onmotion(void (*fn)(YF_coord2 coord, void *data), void *data);
-
-/* Installs a global callback to receive key press/release events. */
-void yf_view_onkey(
-  void (*fn)(int key, int state, unsigned mod_mask, void *data),
-  void *data);
-
-/* Installs a global callback to receive focus in/out events. */
-void yf_view_onfocus(void (*fn)(int in, void *data), void *data);
 
 YF_DECLS_END
 
