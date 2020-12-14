c52ab13dc88f2f40d8bc29ffe632c38414f17ef9
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-13 21:08:36 -0300

    com 'yf-defs.h' additions

--- yf-com/include/yf-defs.h
+++ yf-com/include/yf-defs.h
@@ -17,4 +17,23 @@
 # define YF_DECLS_END
 #endif
 
+/* Compilation attributes. */
+#ifndef YF_UNUSED
+# ifdef __GNUC__
+#  define YF_UNUSED __attribute__ ((unused))
+# else
+#  define YF_UNUSED
+# endif
+#endif
+
+/* Versioning utilities. */
+#define YF_VERSION_MAKE(major, minor, patch) ( \
+  (((major) << 20) & 0xfff00000) | \
+  (((minor) << 10) & 0x000ffc00) | \
+  ((patch) & 0x000003ff) )
+
+#define YF_MAJOR_GET(version) (((version) >> 20) & 0x00000fff)
+#define YF_MINOR_GET(version) (((version) >> 10) & 0x000003ff)
+#define YF_PATCH_GET(version) ((version) & 0x000003ff)
+
 #endif /* YF_DEFS_H */
