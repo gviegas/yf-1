a8d2bc7ba37bf7e938c352e12704ef9480b9e856
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-18 16:01:47 -0300

    core api revision (vertex input)

--- yf-core/include/yf-vinput.h
+++ yf-core/include/yf-vinput.h
@@ -8,45 +8,65 @@
 #ifndef YF_YF_VINPUT_H
 #define YF_YF_VINPUT_H
 
-/* Undefined type format. */
+/**
+ * Undefined type format.
+ */
 #define YF_TYPEFMT_UNDEF 0
 
-/* 1-component formats. */
+/**
+ * 1-component formats.
+ */
 #define YF_TYPEFMT_INT    16
 #define YF_TYPEFMT_UINT   17
 #define YF_TYPEFMT_FLOAT  18
 #define YF_TYPEFMT_DOUBLE 19
 
-/* 2-component formats. */
+/**
+ * 2-component formats.
+ */
 #define YF_TYPEFMT_INT2    32
 #define YF_TYPEFMT_UINT2   33
 #define YF_TYPEFMT_FLOAT2  34
 #define YF_TYPEFMT_DOUBLE2 35
 
-/* 3-component formats. */
+/**
+ * 3-component formats.
+ */
 #define YF_TYPEFMT_INT3    48
 #define YF_TYPEFMT_UINT3   49
 #define YF_TYPEFMT_FLOAT3  50
 #define YF_TYPEFMT_DOUBLE3 51
 
-/* 4-component formats. */
+/**
+ * 4-component formats.
+ */
 #define YF_TYPEFMT_INT4    64
 #define YF_TYPEFMT_UINT4   65
 #define YF_TYPEFMT_FLOAT4  66
 #define YF_TYPEFMT_DOUBLE4 67
 
-/* Type defining a single vertex attribute. */
+/**
+ * Type defining a single vertex attribute.
+ */
 typedef struct {
   unsigned location;
   int typefmt;
   size_t offset;
 } YF_vattr;
 
-/* Vertex input rate. */
+/**
+ * Vertex input rate.
+ */
 #define YF_VRATE_VERT 0
 #define YF_VRATE_INST 1
 
-/* Type defining a vertex input. */
+/**
+ * Type defining a vertex input.
+ *
+ * The vertex input defines a set of attributes residing in the same vertex
+ * buffer. A buffer object bound as vertex buffer during command encoding must
+ * adhere to the input layout.
+ */
 typedef struct {
   const YF_vattr *attrs;
   unsigned attr_n;
