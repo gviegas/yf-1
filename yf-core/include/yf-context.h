19a26d50fd104d1b09a7e04e7375ef7e59735566
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-18 02:10:50 -0300

    core api revision (context)

--- yf-core/include/yf-context.h
+++ yf-core/include/yf-context.h
@@ -5,20 +5,35 @@
  * Copyright © 2020 Gustavo C. Viegas.
  */
 
-#ifndef YF_YF_CONTEXT_HH
-#define YF_YF_CONTEXT_HH
+#ifndef YF_YF_CONTEXT_H
+#define YF_YF_CONTEXT_H
 
 #include <yf/com/yf-defs.h>
 
 YF_DECLS_BEGIN
 
-/* Opaque type defining the execution context. */
+/**
+ * Opaque type defining the execution context.
+ *
+ * This is the main object of yf-core, from which others are created.
+ */
 typedef struct YF_context_o *YF_context;
 
-/* Initializes a new context. */
+/**
+ * Initializes a new context.
+ *
+ * Multiple contexts may not be supported by the implementation.
+ *
+ * @return: On success, returns a new context object. Otherwise, 'NULL' is
+ *  returned and the global error is set to indicate the cause.
+ */
 YF_context yf_context_init(void);
 
-/* Deinitializes a context. */
+/**
+ * Deinitializes a context.
+ *
+ * @param ctx: The context to deinitialize. Can be 'NULL'.
+ */
 void yf_context_deinit(YF_context ctx);
 
 YF_DECLS_END
