cea30ebefda2b7c1e6abd2bf175b8a86bbf67802
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-19 00:57:38 -0300

    ext api revision (ext & filetype)

--- yf-ext/include/yf-filetype.h
+++ yf-ext/include/yf-filetype.h
@@ -8,19 +8,27 @@
 #ifndef YF_YF_FILETYPE_H
 #define YF_YF_FILETYPE_H
 
-/* Generic types. */
+/**
+ * Generic types.
+ */
 #define YF_FILETYPE_UNKNOWN  0
 #define YF_FILETYPE_INTERNAL 1
 
-/* Mesh data. */
+/**
+ * Mesh data.
+ */
 #define YF_FILETYPE_COLLADA 24
 #define YF_FILETYPE_OBJ     25
 
-/* Texture data. */
+/**
+ * Texture data.
+ */
 #define YF_FILETYPE_PNG 48
 #define YF_FILETYPE_BMP 49
 
-/* Font resource. */
+/**
+ * Font resource.
+ */
 #define YF_FILETYPE_TTF 72
 
 #endif /* YF_YF_FILETYPE_H */
