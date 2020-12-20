ce6c1ed5ff9ff2ce3edbf41c88aa284637aedaa1
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-20 00:00:15 -0300

    com api revision (hashing function)

--- yf-com/include/yf-hashfn.h
+++ yf-com/include/yf-hashfn.h
@@ -14,11 +14,20 @@
 
 YF_DECLS_BEGIN
 
-/* Type defining a generic hashing function. */
+/**
+ * Type defining a generic hashing function.
+ */
 typedef size_t (*YF_hashfn)(const void *);
 
-/* Computes a hash value from a pointer.
-   This is the default hashing function, performed on the pointer. */
+/**
+ * Computes a hash value from a pointer.
+ *
+ * This is the default hashing function, which uses the pointer itself to
+ * produce a hash value.
+ *
+ * @param ptr: The pointer.
+ * @return: The hash value.
+ */
 size_t yf_hash(const void *ptr);
 
 YF_DECLS_END
