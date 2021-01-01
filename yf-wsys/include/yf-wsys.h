23ec09c4e7244b29b282f87fa4a7b416589686bb
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-01 16:31:56 -0300

    wsys 'yf-wsys.h'

---
+++ yf-wsys/include/yf-wsys.h
@@ -0,0 +1,20 @@
+/*
+ * YF
+ * yf-wsys.h
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#ifndef YF_YF_WSYS_H
+#define YF_YF_WSYS_H
+
+/**
+ * WSYS interface.
+ */
+#include "yf-event.h"
+#include "yf-keyboard.h"
+#include "yf-platform.h"
+#include "yf-pointer.h"
+#include "yf-window.h"
+
+#endif /* YF_YF_WSYS_H */
