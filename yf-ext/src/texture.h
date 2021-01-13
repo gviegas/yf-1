98eb0efc4c80a189005126c9ec63c2450c72fee1
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-13 17:57:47 -0300

    ext texture setdata()

--- yf-ext/src/texture.h
+++ yf-ext/src/texture.h
@@ -22,6 +22,10 @@ typedef struct {
 /* Initializes a new texture object from texture data directly. */
 YF_texture yf_texture_initdt(const YF_texdt *data);
 
+/* Replaces the contents of a texture object's image. */
+int yf_texture_setdata(YF_texture tex, YF_off2 off, YF_dim2 dim,
+    const void *data);
+
 /* Copies image data from a texture to a dtable resource. */
 int yf_texture_copyres(YF_texture tex, YF_dtable dtb, unsigned alloc_i,
     unsigned binding, unsigned element);
