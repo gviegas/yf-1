5577bce67369f21d1d5f06c5dadb47fac639cbed
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-13 12:31:10 -0300

    ext label getmesh/getstr/setstr functions

--- yf-ext/src/label.c
+++ yf-ext/src/label.c
@@ -6,6 +6,7 @@
  */
 
 #include <stdlib.h>
+#include <wchar.h>
 #include <assert.h>
 
 #include <yf/com/yf-error.h>
@@ -51,18 +52,46 @@ YF_mat4 *yf_label_getxform(YF_label labl) {
 }
 
 YF_mesh yf_label_getmesh(YF_label labl) {
-  /* TODO */
-  assert(0);
+  assert(labl != NULL);
+  return labl->mesh;
 }
 
 wchar_t *yf_label_getstr(YF_label labl, wchar_t *dst, size_t n) {
-  /* TODO */
-  assert(0);
+  assert(labl != NULL);
+  assert(dst != NULL && n > 0);
+
+  if (labl->str == NULL) {
+    dst[0] = L'\0';
+    return dst;
+  }
+  if (wcslen(labl->str) < n)
+    return wcscpy(dst, labl->str);
+  return NULL;
 }
 
-void yf_label_setstr(YF_label labl, wchar_t *str) {
-  /* TODO */
-  assert(0);
+int yf_label_setstr(YF_label labl, wchar_t *str) {
+  assert(labl != NULL);
+
+  if (str == NULL) {
+    free(labl->str);
+    labl->str = NULL;
+    return 0;
+  }
+
+  size_t n = wcslen(labl->str);
+  size_t new_n = wcslen(str);
+
+  if (n != new_n) {
+    void *tmp = realloc(labl->str, sizeof(wchar_t) * (new_n + 1));
+    if (tmp != NULL) {
+      labl->str = tmp;
+    } else if (n < new_n) {
+      yf_seterr(YF_ERR_NOMEM, __func__);
+      return -1;
+    }
+  }
+  wcscpy(labl->str, str);
+  return 0;
 }
 
 void yf_label_deinit(YF_label labl) {
