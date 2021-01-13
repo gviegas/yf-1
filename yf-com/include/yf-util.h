373be3e674de45b1af5c4030d6d43cb85f82e747
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-13 00:34:31 -0300

    com yf-util.h

---
+++ yf-com/include/yf-util.h
@@ -0,0 +1,27 @@
+/*
+ * YF
+ * yf-util.h
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#ifndef YF_YF_UTIL_H
+#define YF_YF_UTIL_H
+
+/**
+ * Computes the minimum of two values.
+ */
+#define YF_MIN(a, b) ((a) < (b) ? (a) : (b))
+
+/**
+ * Computes the maximum of two values.
+ */
+#define YF_MAX(a, b) ((a) > (b) ? (a) : (b))
+
+/**
+ * Clamps 'x' to the ['min','max'] range.
+ */
+#define YF_CLAMP(x, min, max) \
+  ((x) < (min) ? (min) : ((x) > (max) ? (max) : (x)))
+
+#endif /* YF_YF_UTIL_H */
