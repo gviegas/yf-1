bc2e3914c2c8d0c10a35ebc67a28011c0773d339
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-17 23:16:44 -0300

    ext print

---
+++ yf-ext/test/print.c
@@ -0,0 +1,15 @@
+/*
+ * YF
+ * print.c
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#include <stdio.h>
+
+#include "print.h"
+
+#undef YF_PTITLE
+#define YF_PTITLE printf("\n[YF] OUTPUT (%s):", __func__);
+
+/* TODO */
---
+++ yf-ext/test/print.h
@@ -0,0 +1,13 @@
+/*
+ * YF
+ * print.h
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#ifndef YF_PRINT_H
+#define YF_PRINT_H
+
+/* TODO */
+
+#endif /* YF_PRINT_H */
