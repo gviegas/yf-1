e74d47abe8d37bf89a30d1687aa2619c7a1ec6ea
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-26 12:13:36 -0300

    ext test-particle

---
+++ yf-ext/test/test-particle.c
@@ -0,0 +1,91 @@
+/*
+ * YF
+ * test-particle.c
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+#include <yf/wsys/yf-event.h>
+#include <yf/wsys/yf-keyboard.h>
+
+#include "yf-ext.h"
+
+#define YF_WINW 960
+#define YF_WINH 600
+#define YF_WINT "Particle"
+#define YF_FPS  30
+
+/* Local variables. */
+struct L_vars {
+  YF_window win;
+  YF_view view;
+  YF_scene scn;
+  YF_particle part;
+  YF_texture tex;
+
+  struct {
+    int quit;
+  } input;
+};
+static struct L_vars l_vars = {0};
+
+/* Handles key events. */
+static void on_key(int key, int state,
+    YF_UNUSED unsigned mod_mask, YF_UNUSED void *arg)
+{
+  /* TODO */
+  switch (key) {
+    default:
+      l_vars.input.quit = 1;
+  }
+}
+
+/* Updates content. */
+static void update(double elapsed_time) {
+  printf("update (%.4f)\n", elapsed_time);
+
+  if (l_vars.input.quit) {
+    printf("quit\n");
+    yf_view_stop(l_vars.view);
+  }
+
+  /* TODO */
+}
+
+/* Tests particle rendering. */
+int yf_test_particle(void) {
+  YF_evtfn evtfn = {.key_kb = on_key};
+  yf_setevtfn(YF_EVT_KEYKB, evtfn, NULL);
+
+  l_vars.win = yf_window_init(YF_WINW, YF_WINH, YF_WINT, 0);
+  assert(l_vars.win != NULL);
+
+  l_vars.view = yf_view_init(l_vars.win);
+  assert(l_vars.view != NULL);
+
+  l_vars.scn = yf_scene_init();
+  assert(l_vars.scn != NULL);
+
+  l_vars.part = yf_particle_init(1000);
+  assert(l_vars.part != NULL);
+
+  l_vars.tex = yf_texture_init(YF_FILETYPE_BMP, "tmp/sprite.bmp");
+  assert(l_vars.tex != NULL);
+
+  yf_particle_settex(l_vars.part, l_vars.tex);
+
+  yf_node_insert(yf_scene_getnode(l_vars.scn),
+      yf_particle_getnode(l_vars.part));
+
+  yf_view_setscene(l_vars.view, l_vars.scn);
+
+  if (yf_view_start(l_vars.view, YF_FPS, update) != 0)
+    assert(0);
+
+  /* TODO: Deinitialization. */
+
+  return 0;
+}
