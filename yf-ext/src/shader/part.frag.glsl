6ab8fcf9dc6d38802d203ddb5e118d5dc33a7994
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-26 13:03:36 -0300

    ext particle fragment shader

---
+++ yf-ext/src/shader/part.frag.glsl
@@ -0,0 +1,20 @@
+/*
+ * YF
+ * part.frag.glsl
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#version 460 core
+
+layout(set=1, binding=1) uniform sampler2D u_tex;
+
+in IO_vtx {
+  layout(location=3) vec4 clr;
+} in_vtx;
+
+layout(location=0) out vec4 clr0;
+
+void main() {
+  clr0 = in_vtx.clr * textureLod(u_tex, gl_PointCoord, 0.0);
+}
