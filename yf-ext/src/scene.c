9fe201a3e6743f48a1b28efca022f1120f0dabce
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-27 12:31:39 -0300

    ext scene traverse_scn() updated to handle quad objects

--- yf-ext/src/scene.c
+++ yf-ext/src/scene.c
@@ -501,8 +501,11 @@ static int traverse_scn(YF_node node, void *arg) {
       break;
 
     case YF_NODEOBJ_QUAD:
-      /* TODO */
-      assert(0);
+      if (yf_list_insert(l_vars.quads, obj) != 0) {
+        *(int *)arg = -1;
+        return -1;
+      }
+      break;
 
     case YF_NODEOBJ_LABEL:
       /* TODO */
