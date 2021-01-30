3542a7ff37f570acc2c9236c55ddd4cca139cbf6
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-01-30 19:06:27 -0300

    core dtable minor layout correction on dtype conversion macro

--- yf-core/src/dtable.h
+++ yf-core/src/dtable.h
@@ -2,7 +2,7 @@
  * YF
  * dtable.h
  *
- * Copyright © 2020 Gustavo C. Viegas.
+ * Copyright © 2020-2021 Gustavo C. Viegas.
  */
 
 #ifndef YF_DTABLE_H
@@ -38,19 +38,26 @@ typedef struct YF_dtable_o {
 /* Converts from a 'YF_DTYPE' value. */
 #define YF_DTYPE_FROM(dtp, to) do { \
   switch (dtp) { \
-  case YF_DTYPE_UNIFORM: \
-    to = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER; break; \
-  case YF_DTYPE_MUTABLE: \
-    to = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER; break; \
-  case YF_DTYPE_IMAGE: \
-    to = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE; break; \
-  case YF_DTYPE_SAMPLED: \
-    to = VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE; break; \
-  case YF_DTYPE_SAMPLER: \
-    to = VK_DESCRIPTOR_TYPE_SAMPLER; break; \
-  case YF_DTYPE_ISAMPLER: \
-    to = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER; break; \
-  default: to = INT_MAX; \
+    case YF_DTYPE_UNIFORM: \
+      to = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER; \
+      break; \
+    case YF_DTYPE_MUTABLE: \
+      to = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER; \
+      break; \
+    case YF_DTYPE_IMAGE: \
+      to = VK_DESCRIPTOR_TYPE_STORAGE_IMAGE; \
+      break; \
+    case YF_DTYPE_SAMPLED: \
+      to = VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE; \
+      break; \
+    case YF_DTYPE_SAMPLER: \
+      to = VK_DESCRIPTOR_TYPE_SAMPLER; \
+      break; \
+    case YF_DTYPE_ISAMPLER: \
+      to = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER; \
+      break; \
+    default: \
+      to = INT_MAX; \
   } } while (0)
 
 #endif /* YF_DTABLE_H */
