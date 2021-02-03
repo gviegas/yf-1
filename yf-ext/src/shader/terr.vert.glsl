5015817b415114d2fc4f946952476822762c0cca
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-02-03 14:37:24 -0300

    ext terrain shaders updated

--- yf-ext/src/shader/terr.vert.glsl
+++ yf-ext/src/shader/terr.vert.glsl
@@ -7,16 +7,41 @@
 
 #version 460 core
 
+layout(std140, column_major) uniform;
+
+/**
+ * Type defining a viewport.
+ */
+struct T_vport {
+  float x;
+  float y;
+  float wdt;
+  float hgt;
+  float near;
+  float far;
+};
+
+/**
+ * Global uniform data.
+ */
 layout(set=0, binding=0) uniform U_glob {
   mat4 v;
   mat4 p;
+  mat4 o;
+  T_vport vport[1];
 } u_glob;
 
+/**
+ * Instance's uniform data.
+ */
 layout(set=1, binding=0) uniform U_inst {
   mat4 m;
-  mat4 mvp;
+  mat4 mv;
 } u_inst;
 
+/**
+ * Height map.
+ */
 layout(set=1, binding=3) uniform sampler2D u_hmap;
 
 layout(location=0) in vec3 pos;
@@ -30,7 +55,7 @@ out IO_vtx {
 
 void main() {
   float y = -textureLod(u_hmap, tc, 0.0).r;
-  gl_Position = u_inst.mvp * vec4(pos.x, y, pos.z, 1.0);
+  gl_Position = u_glob.p * u_inst.mv * vec4(pos.x, y, pos.z, 1.0);
   out_vtx.tc = tc;
   out_vtx.norm = norm;
 }
