56acf7060949c1014ac8069656b3b4e13d5a3c23
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-02-14 12:12:40 -0300

    ext data-gltf 'meshes' parsing now uses parse_str()

--- yf-ext/src/data-gltf.c
+++ yf-ext/src/data-gltf.c
@@ -1248,14 +1248,8 @@ static int parse_meshes(FILE *file, L_symbol *symbol,
             }
           } while (symbol->symbol != YF_SYMBOL_OP || symbol->tokens[0] != ']');
         } else if (strcmp("name", symbol->tokens) == 0) {
-          next_symbol(file, symbol); /* : */
-          next_symbol(file, symbol);
-          meshes->v[index].name = malloc(1+strlen(symbol->tokens));
-          if (meshes->v[index].name == NULL) {
-            yf_seterr(YF_ERR_NOMEM, __func__);
+          if (parse_str(file, symbol, &meshes->v[index].name) != 0)
             return -1;
-          }
-          strcpy(meshes->v[index].name, symbol->tokens);
         } else {
           if (consume_prop(file, symbol) != 0)
             return -1;
