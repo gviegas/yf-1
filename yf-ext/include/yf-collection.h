2f2028dbca956a921854834120046e7dfd15832c
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-03-08 15:03:06 -0300

    ext collection

---
+++ yf-ext/include/yf-collection.h
@@ -0,0 +1,57 @@
+/*
+ * YF
+ * yf-collection.h
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#ifndef YF_YF_COLLECTION_H
+#define YF_YF_COLLECTION_H
+
+#include <yf/com/yf-defs.h>
+
+YF_DECLS_BEGIN
+
+/**
+ * Opaque type defining a collection of resources.
+ */
+typedef struct YF_collection_o *YF_collection;
+
+/**
+ * Types of resources that a collection can store.
+ */
+#define YF_COLLRES_MESH    0
+#define YF_COLLRES_TEXTURE 1
+#define YF_COLLRES_FONT    2
+
+/**
+ * Initializes a new collection.
+ *
+ * @param pathname: The pathname of a file containing resources to load
+ *  during initialization. Can be 'NULL'.
+ * @return: On success, returns a new collection. Otherwise, 'NULL' is
+ *  returned and the global error is set to indicate the cause.
+ */
+YF_collection yf_collection_init(const char *pathname);
+
+/**
+ * Gets a resource stored in a collection.
+ *
+ * @param coll: The collection.
+ * @param collres: The 'YF_COLLRES' value indicating the type of the resource.
+ * @param name: The name of the resource.
+ * @return: On success, returns the resource. Otherwise, 'NULL' is returned
+ *  and the global error is set to indicate the cause.
+ */
+void *yf_collection_getres(YF_collection coll, int collres, const char *name);
+
+/**
+ * Deinitializes a collection.
+ *
+ * @param coll: The collection to deinitialize. Can be 'NULL'.
+ */
+void yf_collection_deinit(YF_collection coll);
+
+YF_DECLS_END
+
+#endif /* YF_YF_COLLECTION_H */
---
+++ yf-ext/src/collection.c
@@ -0,0 +1,33 @@
+/*
+ * YF
+ * collection.c
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#include <stdlib.h>
+#include <assert.h>
+
+#include <yf/com/yf-error.h>
+
+#include "collection.h"
+
+struct YF_collection {
+  /* TODO */
+  size_t n;
+};
+
+YF_collection yf_collection_init(const char *pathname) {
+  /* TODO */
+  assert(0);
+}
+
+void *yf_collection_getres(YF_collection coll, int collres, const char *name) {
+  /* TODO */
+  assert(0);
+}
+
+void yf_collection_deinit(YF_collection coll) {
+  /* TODO */
+  assert(0);
+}
