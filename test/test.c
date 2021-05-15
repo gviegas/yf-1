069f9c9ebbdbb6835901b40f82b34d1ec8d49aa6
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-05-15 14:27:35 -0300

    test indenting

--- test/test.c
+++ test/test.c
@@ -18,22 +18,22 @@
 
 int main(int argc, char *argv[])
 {
-  char line[YF_LINESZ+1];
-  memset(line, YF_LINECHR, YF_LINESZ);
-  line[YF_LINESZ] = '\0';
+    char line[YF_LINESZ+1];
+    memset(line, YF_LINECHR, YF_LINESZ);
+    line[YF_LINESZ] = '\0';
 
-  printf("%s\n[YF][%s] - Test\n%s\n\n", line, yf_g_test.name, line);
+    printf("%s\n[YF][%s] - Test\n%s\n\n", line, yf_g_test.name, line);
 
-  if (argc == 1) {
-    printf("! Error: missing test ID\n");
-    printf("! Usage: %s TEST_ID [parameters]\n", argv[0]);
-    printf("\nPossible values for TEST_ID:\n");
-    for (size_t i = 0; i < yf_g_test.id_n; ++i)
-      printf("* %s\n", yf_g_test.ids[i]);
-    printf("\n! No tests executed\n");
-  } else {
-    yf_g_test.fn(argc-1, argv+1);
-  }
+    if (argc == 1) {
+        printf("! Error: missing test ID\n");
+        printf("! Usage: %s TEST_ID [parameters]\n", argv[0]);
+        printf("\nPossible values for TEST_ID:\n");
+        for (size_t i = 0; i < yf_g_test.id_n; ++i)
+            printf("* %s\n", yf_g_test.ids[i]);
+        printf("\n! No tests executed\n");
+    } else {
+        yf_g_test.fn(argc-1, argv+1);
+    }
 
-  printf("\n%s\nEnd of test\n%s\n", line, line);
+    printf("\n%s\nEnd of test\n%s\n", line, line);
 }
