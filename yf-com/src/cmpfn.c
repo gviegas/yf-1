9b8f5015bbe4e66394a5acc2635cf1efb9df5eaf
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-05-22 15:34:38 -0300

    com cmpstr()

--- yf-com/src/cmpfn.c
+++ yf-com/src/cmpfn.c
@@ -6,10 +6,16 @@
  */
 
 #include <stddef.h>
+#include <string.h>
 
 #include "cmpfn.h"
 
 int yf_cmp(const void *ptr1, const void *ptr2)
 {
     return (ptrdiff_t)ptr1 - (ptrdiff_t)ptr2;
+}
+
+int yf_cmpstr(const void *str1, const void *str2)
+{
+    return strcmp(str1, str2);
 }
