c8f70ad24aa2bcec6f1cbb6748cbe35de433f214
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-05-23 13:00:05 -0300

    com pubsub now uses dictionary instead of hashset

--- yf-com/src/pubsub.c
+++ yf-com/src/pubsub.c
@@ -9,32 +9,26 @@
 #include <assert.h>
 
 #include "pubsub.h"
-#include "hashset.h"
+#include "dict.h"
 #include "error.h"
 
 /* TODO: Thread-safe. */
 
 /* Publisher variables. */
 typedef struct {
-    const void *pub;
     unsigned pubsub_mask;
-    YF_hashset subs;
+    YF_dict subs;
 } T_pub;
 
 /* Subscriber variables. */
 typedef struct {
-    const void *sub;
     unsigned pubsub_mask;
     void (*callb)(void *, int, void *);
     void *arg;
 } T_sub;
 
-/* Hashset containing all publishers. */
-static YF_hashset l_pubs = NULL;
-
-/* Hashset functions for pub/sub. */
-static size_t hash_ps(const void *x);
-static int cmp_ps(const void *a, const void *b);
+/* Dictionary containing all publishers. */
+static YF_dict l_pubs = NULL;
 
 int yf_setpub(const void *pub, unsigned pubsub_mask)
 {
@@ -43,22 +37,26 @@ int yf_setpub(const void *pub, unsigned pubsub_mask)
         return -1;
     }
 
-    if (l_pubs == NULL && (l_pubs = yf_hashset_init(hash_ps, cmp_ps)) == NULL)
+    if (l_pubs == NULL && (l_pubs = yf_dict_init(NULL, NULL)) == NULL)
         return -1;
 
-    const T_pub key = {pub, 0, NULL};
-    T_pub *val = yf_hashset_search(l_pubs, &key);
+    T_pub *val = yf_dict_search(l_pubs, pub);
 
     /* removal */
     if (pubsub_mask == YF_PUBSUB_NONE) {
         if (val != NULL) {
-            yf_hashset_remove(l_pubs, val);
+            yf_dict_remove(l_pubs, pub);
+
+            YF_iter it = YF_NILIT;
             T_sub *sub;
-            while ((sub = yf_hashset_extract(val->subs, NULL)) != NULL)
+
+            while ((sub = yf_dict_next(val->subs, &it, NULL)) != NULL)
                 free(sub);
-            yf_hashset_deinit(val->subs);
+
+            yf_dict_deinit(val->subs);
             free(val);
         }
+
         return 0;
     }
 
@@ -68,19 +66,23 @@ int yf_setpub(const void *pub, unsigned pubsub_mask)
             yf_seterr(YF_ERR_NOMEM, __func__);
             return -1;
         }
-        val->pub = pub;
-        val->subs = yf_hashset_init(hash_ps, cmp_ps);
+
+        val->subs = yf_dict_init(NULL, NULL);
+
         if (val->subs == NULL) {
             free(val);
             return -1;
         }
-        if (yf_hashset_insert(l_pubs, val) != 0) {
-            yf_hashset_deinit(val->subs);
+
+        if (yf_dict_insert(l_pubs, pub, val) != 0) {
+            yf_dict_deinit(val->subs);
             free(val);
             return -1;
         }
     }
+
     val->pubsub_mask = pubsub_mask;
+
     return 0;
 }
 
@@ -91,8 +93,7 @@ unsigned yf_checkpub(const void *pub)
     if (l_pubs == NULL)
         return YF_PUBSUB_NONE;
 
-    const T_pub key = {pub, 0, NULL};
-    T_pub *val = yf_hashset_search(l_pubs, &key);
+    T_pub *val = yf_dict_search(l_pubs, pub);
 
     return val != NULL ? val->pubsub_mask : YF_PUBSUB_NONE;
 }
@@ -102,14 +103,15 @@ void yf_publish(const void *pub, int pubsub)
     assert(pub != NULL);
     assert(l_pubs != NULL);
 
-    const T_pub key = {pub, 0, NULL};
-    T_pub *val = yf_hashset_search(l_pubs, &key);
+    T_pub *val = yf_dict_search(l_pubs, pub);
+
     if (val == NULL)
         return;
 
     YF_iter it = YF_NILIT;
     T_sub *sub;
-    while ((sub = yf_hashset_next(val->subs, &it)) != NULL) {
+
+    while ((sub = yf_dict_next(val->subs, &it, NULL)) != NULL) {
         if (sub->pubsub_mask & pubsub)
             sub->callb((void *)pub, pubsub, sub->arg);
     }
@@ -126,19 +128,20 @@ int yf_subscribe(const void *pub, const void *sub, unsigned pubsub_mask,
         return -1;
     }
 
-    const T_pub pk = {pub, 0, NULL};
-    T_pub *pv = yf_hashset_search(l_pubs, &pk);
+    T_pub *pv = yf_dict_search(l_pubs, pub);
+
     if (pv == NULL)
         return -1;
-    const T_sub sk = {sub, 0, NULL, NULL};
-    T_sub *sv = yf_hashset_search(pv->subs, &sk);
+
+    T_sub *sv = yf_dict_search(pv->subs, sub);
 
     /* removal */
     if (pubsub_mask == YF_PUBSUB_NONE) {
         if (sv != NULL) {
-            yf_hashset_remove(pv->subs, sv);
+            yf_dict_remove(pv->subs, sub);
             free(sv);
         }
+
         return 0;
     }
 
@@ -148,28 +151,17 @@ int yf_subscribe(const void *pub, const void *sub, unsigned pubsub_mask,
             yf_seterr(YF_ERR_NOMEM, __func__);
             return -1;
         }
-        sv->sub = sub;
-        if (yf_hashset_insert(pv->subs, sv) != 0) {
+
+        if (yf_dict_insert(pv->subs, sub, sv) != 0) {
             free(sv);
             return -1;
         }
     }
+
     /* XXX: May want to compare pub/sub masks. */
     sv->pubsub_mask = pubsub_mask;
     sv->callb = callb;
     sv->arg = arg;
+
     return 0;
-}
-
-static size_t hash_ps(const void *x)
-{
-    const void *k = *(void **)x;
-    return (size_t)k;
-}
-
-static int cmp_ps(const void *a, const void *b)
-{
-    const void *k1 = *(void **)a;
-    const void *k2 = *(void **)b;
-    return (ptrdiff_t)k1 - (ptrdiff_t)k2;
 }
