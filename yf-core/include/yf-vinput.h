9b97d9b917040c0239a549f7e204bcdee2d54057
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-06-25 20:15:49 -0300

    core vinput TYPEFMT renamed VFMT

--- yf-core/include/yf-vinput.h
+++ yf-core/include/yf-vinput.h
@@ -9,48 +9,48 @@
 #define YF_YF_VINPUT_H
 
 /**
- * Undefined type format.
+ * Undefined vertex format.
  */
-#define YF_TYPEFMT_UNDEF 0
+#define YF_VFMT_UNDEF 0
 
 /**
  * 1-component formats.
  */
-#define YF_TYPEFMT_INT    16
-#define YF_TYPEFMT_UINT   17
-#define YF_TYPEFMT_FLOAT  18
-#define YF_TYPEFMT_DOUBLE 19
+#define YF_VFMT_INT    16
+#define YF_VFMT_UINT   17
+#define YF_VFMT_FLOAT  18
+#define YF_VFMT_DOUBLE 19
 
 /**
  * 2-component formats.
  */
-#define YF_TYPEFMT_INT2    32
-#define YF_TYPEFMT_UINT2   33
-#define YF_TYPEFMT_FLOAT2  34
-#define YF_TYPEFMT_DOUBLE2 35
+#define YF_VFMT_INT2    32
+#define YF_VFMT_UINT2   33
+#define YF_VFMT_FLOAT2  34
+#define YF_VFMT_DOUBLE2 35
 
 /**
  * 3-component formats.
  */
-#define YF_TYPEFMT_INT3    48
-#define YF_TYPEFMT_UINT3   49
-#define YF_TYPEFMT_FLOAT3  50
-#define YF_TYPEFMT_DOUBLE3 51
+#define YF_VFMT_INT3    48
+#define YF_VFMT_UINT3   49
+#define YF_VFMT_FLOAT3  50
+#define YF_VFMT_DOUBLE3 51
 
 /**
  * 4-component formats.
  */
-#define YF_TYPEFMT_INT4    64
-#define YF_TYPEFMT_UINT4   65
-#define YF_TYPEFMT_FLOAT4  66
-#define YF_TYPEFMT_DOUBLE4 67
+#define YF_VFMT_INT4    64
+#define YF_VFMT_UINT4   65
+#define YF_VFMT_FLOAT4  66
+#define YF_VFMT_DOUBLE4 67
 
 /**
  * Type defining a single vertex attribute.
  */
 typedef struct {
     unsigned location;
-    int typefmt;
+    int vfmt;
     size_t offset;
 } YF_vattr;
 
