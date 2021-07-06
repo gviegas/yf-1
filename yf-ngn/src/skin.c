778fd412ffff70367ff042e393b078a1082dda1f
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-06 16:38:42 -0300

    ngn skin_makeskel()

--- yf-ngn/src/skin.c
+++ yf-ngn/src/skin.c
@@ -7,14 +7,21 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "yf/com/yf-error.h"
 
 #include "yf-skin.h"
+#include "yf-node.h"
 
 struct YF_skin_o {
     YF_joint *jnts;
     unsigned jnt_n;
+};
+
+struct YF_skeleton_o {
+    YF_node *nodes;
+    unsigned node_n;
 };
 
 YF_skin yf_skin_init(const YF_joint *jnts, unsigned jnt_n)
@@ -40,6 +47,54 @@ YF_skin yf_skin_init(const YF_joint *jnts, unsigned jnt_n)
     skin->jnt_n = jnt_n;
 
     return skin;
+}
+
+YF_skeleton yf_skin_makeskel(YF_skin skin)
+{
+    assert(skin != NULL);
+
+    YF_skeleton skel = malloc(sizeof(struct YF_skeleton_o));
+    if (skel == NULL) {
+        yf_seterr(YF_ERR_NOMEM, __func__);
+        return NULL;
+    }
+
+    skel->node_n = skin->jnt_n + 1;
+    skel->nodes = malloc(skel->node_n * sizeof *skel->nodes);
+    if (skel->nodes == NULL) {
+        yf_seterr(YF_ERR_NOMEM, __func__);
+        free(skel);
+        return NULL;
+    }
+
+    for (unsigned i = 0; i < skel->node_n; i++) {
+        if ((skel->nodes[i] = yf_node_init()) == NULL) {
+            for (unsigned j = 0; j < i; j++)
+                yf_node_deinit(skel->nodes[j]);
+            free(skel->nodes);
+            free(skel);
+            return NULL;
+        }
+    }
+
+    for (unsigned i = 0; i < skin->jnt_n; i++) {
+        YF_node node = skel->nodes[i];
+        YF_joint *jnt = skin->jnts+i;
+
+        yf_mat4_copy(*yf_node_getxform(node), jnt->xform);
+        /* XXX: Make sure name is null-terminated (on init()). */
+        yf_node_setname(node, jnt->name);
+
+        /* XXX: Make sure index is less than joint count (on init()). */
+        if (jnt->pnt_i < 0)
+            yf_node_insert(skel->nodes[skin->jnt_n], node);
+        else
+            yf_node_insert(skel->nodes[jnt->pnt_i], node);
+    }
+
+    assert(!yf_node_isleaf(skel->nodes[skin->jnt_n]));
+
+    return skel;
 }
 
 void yf_skin_deinit(YF_skin skin)
