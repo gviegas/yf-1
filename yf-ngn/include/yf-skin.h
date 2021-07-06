ba8b74ab49af0d4c09dab6a8b61503839ec86cf2
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-06 15:13:20 -0300

    ngn skeleton

--- yf-ngn/include/yf-skin.h
+++ yf-ngn/include/yf-skin.h
@@ -17,10 +17,14 @@ YF_DECLS_BEGIN
 /* Opaque type defining a skin. */
 typedef struct YF_skin_o *YF_skin;
 
-/* Type defining a joint. */
+/* Opaque type defining an instance of a skin's joint hierarchy. */
+typedef struct YF_skeleton_o *YF_skeleton;
+
+/* Type describing a joint for skinning. */
 typedef struct {
     YF_mat4 xform;
     YF_mat4 ibm;
+    char name[32];
     /* Index of the joint's parent in the array used to initialize a skin.
        A negative value indicates that the joint has no parent. */
     long pnt_i;
@@ -35,6 +39,23 @@ typedef struct {
  *  the global error is set to indicate the cause.
  */
 YF_skin yf_skin_init(const YF_joint *jnts, unsigned jnt_n);
+
+/**
+ * Makes a new skeleton from a given skin.
+ *
+ * @param skin: The skin.
+ * @return: On success, returns a new skeleton. Otherwise, 'NULL' is returned
+ *  and the global error is set to indicate the cause.
+ */
+YF_skeleton yf_skin_makeskel(YF_skin skin);
+
+/**
+ * Unmakes a skin's skeleton.
+ *
+ * @param skin: The skin that produced 'skel'.
+ * @param skel: The skeleton to unmake.
+ */
+void yf_skin_unmkskel(YF_skin skin, YF_skeleton skel);
 
 /**
  * Deinitializes a skin.
