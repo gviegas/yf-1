cb8fe6d07ca229a1cf4972a0a04813f13459a102
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-13 14:42:14 -0300

    ngn data-gltf loadgltf2() corrected call to load_material()

--- yf-ngn/src/data-gltf.c
+++ yf-ngn/src/data-gltf.c
@@ -3768,7 +3768,7 @@ int yf_loadgltf2(FILE *file, size_t index, int datac, YF_datac *dst)
         r = load_skin(&gltf, &fdata, &cont, index);
         break;
     case YF_DATAC_MATL:
-        r = load_material(&gltf, &fdata, index, &dst->matl, NULL);
+        r = load_material(&gltf, &fdata, &cont, index);
         break;
     default:
         yf_seterr(YF_ERR_INVARG, __func__);
