8516d4022496b189c28ec9948671c24425d8e759
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-13 12:17:18 -0300

    ngn data-gltf loadgltf() corrected call to load_mesh()

--- yf-ngn/src/data-gltf.c
+++ yf-ngn/src/data-gltf.c
@@ -3783,7 +3783,7 @@ int yf_loadgltf(const char *pathname, size_t index, int datac, YF_datac *dst)
         r = load_contents(&gltf, &fdata, dst->coll);
         break;
     case YF_DATAC_MESH:
-        r = load_mesh(&gltf, &fdata, index, &dst->mesh, NULL);
+        r = load_mesh(&gltf, &fdata, &cont, index);
         break;
     case YF_DATAC_TEX:
         r = load_texture(&gltf, &fdata, index, &dst->tex, NULL);
