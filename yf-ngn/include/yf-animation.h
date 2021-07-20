146f9b06f64397c0db315198bcb91238f613ea10
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-20 15:13:05 -0300

    ngn animation

---
+++ yf-ngn/include/yf-animation.h
@@ -0,0 +1,37 @@
+/*
+ * YF
+ * yf-animation.h
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#ifndef YF_YF_ANIMATION_H
+#define YF_YF_ANIMATION_H
+
+#include "yf/com/yf-defs.h"
+
+YF_DECLS_BEGIN
+
+/**
+ * Opaque type defining keyframe animation.
+ */
+typedef struct YF_animation_o *YF_animation;
+
+/**
+ * Initializes a new animation.
+ *
+ * @return: On success, returns a new animation. Otherwise, 'NULL' is returned
+ *  and the global error is set to indicate the cause.
+ */
+YF_animation yf_animation_init(void);
+
+/**
+ * Deinitializes an animation.
+ *
+ * @param anim: The animation to deinitialize. Can be 'NULL'.
+ */
+void yf_animation_deinit(YF_animation anim);
+
+YF_DECLS_END
+
+#endif /* YF_YF_ANIMATION_H */
