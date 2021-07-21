e43a396967bd35b6d3ad95cff91c41a410dc9ddf
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-21 15:37:03 -0300

    ngn animation_init() imp.

--- yf-ngn/src/animation.c
+++ yf-ngn/src/animation.c
@@ -9,6 +9,8 @@
 #include <string.h>
 #include <assert.h>
 
+#include "yf/com/yf-error.h"
+
 #include "yf-animation.h"
 
 struct YF_animation_o {
@@ -25,8 +27,108 @@ YF_animation yf_animation_init(const YF_kfinput *inputs, unsigned input_n,
                                const YF_kfoutput *outputs, unsigned output_n,
                                const YF_kfaction *actions, unsigned action_n)
 {
-    /* TODO */
-    return NULL;
+    assert(inputs != NULL && input_n > 0);
+    assert(outputs != NULL && output_n > 0);
+    assert(actions != NULL && action_n > 0);
+
+    YF_animation anim = calloc(1, sizeof(struct YF_animation_o));
+    if (anim == NULL) {
+        yf_seterr(YF_ERR_NOMEM, __func__);
+        return NULL;
+    }
+
+    /* inputs */
+    anim->inputs = calloc(input_n, sizeof *inputs);
+    if (anim->inputs == NULL) {
+        yf_seterr(YF_ERR_NOMEM, __func__);
+        yf_animation_deinit(anim);
+        return NULL;
+    }
+    anim->input_n = input_n;
+
+    for (unsigned i = 0; i < input_n; i++) {
+        assert(inputs[i].timeline != NULL && inputs[i].n > 0);
+
+        const size_t sz = inputs[i].n * sizeof *inputs[i].timeline;
+        anim->inputs[i].timeline = malloc(sz);
+        if (anim->inputs[i].timeline == NULL) {
+            yf_seterr(YF_ERR_NOMEM, __func__);
+            yf_animation_deinit(anim);
+            return NULL;
+        }
+        memcpy(anim->inputs[i].timeline, inputs[i].timeline, sz);
+        anim->inputs[i].n = inputs[i].n;
+    }
+
+    /* outputs */
+    anim->outputs = calloc(output_n, sizeof *outputs);
+    if (anim->outputs == NULL) {
+        yf_seterr(YF_ERR_NOMEM, __func__);
+        yf_animation_deinit(anim);
+        return NULL;
+    }
+    anim->output_n = output_n;
+
+    for (unsigned i = 0; i < output_n; i++) {
+        assert(outputs[i].n > 0);
+
+        size_t sz;
+        switch (outputs[i].kfprop) {
+        case YF_KFPROP_T:
+            sz = outputs[i].n * sizeof *outputs[i].t;
+            anim->outputs[i].t = malloc(sz);
+            if (anim->outputs[i].t == NULL) {
+                yf_seterr(YF_ERR_NOMEM, __func__);
+                yf_animation_deinit(anim);
+                return NULL;
+            }
+            memcpy(anim->outputs[i].t, outputs[i].t, sz);
+            break;
+
+        case YF_KFPROP_R:
+            sz = outputs[i].n * sizeof *outputs[i].r;
+            anim->outputs[i].r = malloc(sz);
+            if (anim->outputs[i].r == NULL) {
+                yf_seterr(YF_ERR_NOMEM, __func__);
+                yf_animation_deinit(anim);
+                return NULL;
+            }
+            memcpy(anim->outputs[i].r, outputs[i].r, sz);
+            break;
+
+        case YF_KFPROP_S:
+            sz = outputs[i].n * sizeof *outputs[i].s;
+            anim->outputs[i].s = malloc(sz);
+            if (anim->outputs[i].s == NULL) {
+                yf_seterr(YF_ERR_NOMEM, __func__);
+                yf_animation_deinit(anim);
+                return NULL;
+            }
+            memcpy(anim->outputs[i].s, outputs[i].s, sz);
+            break;
+
+        default:
+            assert(0);
+            yf_seterr(YF_ERR_INVARG, __func__);
+            yf_animation_deinit(anim);
+            return NULL;
+        }
+
+        anim->outputs[i].kfprop = outputs[i].kfprop;
+        anim->outputs[i].n = outputs[i].n;
+    }
+
+    /* actions */
+    anim->actions = malloc(action_n * sizeof *actions);
+    if (anim->actions == NULL) {
+        yf_seterr(YF_ERR_NOMEM, __func__);
+        yf_animation_deinit(anim);
+        return NULL;
+    }
+    memcpy(anim->actions, actions, action_n * sizeof *actions);
+    anim->action_n = action_n;
+
+    return anim;
 }
 
 void yf_animation_deinit(YF_animation anim)
