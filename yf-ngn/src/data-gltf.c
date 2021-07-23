03ff53130927c1fbb76981dff9f954c69b5a9de0
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-23 14:14:32 -0300

    ngn data-gltf load_animation() action targets

--- yf-ngn/src/data-gltf.c
+++ yf-ngn/src/data-gltf.c
@@ -4045,8 +4045,18 @@ static int load_animation(const T_gltf *gltf, T_fdata *fdata, T_cont *cont,
 
 #undef YF_DEALLOCKF
 
-    /* TODO... */
+    for (size_t i = 0; i < channel_n; i++) {
+        const T_int node = channels->v[i].target.node;
+        if (node == YF_INT_MIN)
+            continue;
 
+        if (cont->nodes[node] == NULL ||
+            load_node(gltf, fdata, cont, node) != 0)
+            return -1;
+
+        /* XXX: Caller is responsible for creating the node hierarchy. */
+        yf_animation_settarget(cont->anims[animation], i, cont->nodes[node]);
+    }
     return 0;
 }
 
