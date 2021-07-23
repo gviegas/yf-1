bdd23e845d9c706eb169be666ae74f67f189ffa6
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-23 19:54:55 -0300

    ngn animation_gettarget()

--- yf-ngn/include/yf-animation.h
+++ yf-ngn/include/yf-animation.h
@@ -85,6 +85,17 @@ YF_animation yf_animation_init(const YF_kfinput *inputs, unsigned input_n,
                                const YF_kfaction *actions, unsigned action_n);
 
 /**
+ * Gets the target of an animation's action.
+ *
+ * @param anim: The animation.
+ * @param action: The index in the 'YF_kfaction' array of 'anim' indicating
+ *  the action whose target is to be retrieved.
+ * @return: The node currently set as target for 'action', or 'NULL' if none
+ *  is set.
+ */
+YF_node yf_animation_gettarget(YF_animation anim, unsigned action);
+
+/**
  * Sets the target of an animation's action.
  *
  * @param anim: The animation.
