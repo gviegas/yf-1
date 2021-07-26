cf01462ab8879320f8baff4f911d0a50d7b7d9ed
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-26 16:03:25 -0300

    ngn data-gltf load_mesh() default attr. values

--- yf-ngn/src/data-gltf.c
+++ yf-ngn/src/data-gltf.c
@@ -3104,8 +3104,26 @@ static int load_mesh(const T_gltf *gltf, T_fdata *fdata, T_cont *cont,
     }
 
     for (size_t i = 0; i < YF_GLTF_ATTR_N; i++) {
-        if (attrs[i].acc == YF_INT_MIN)
+        if (attrs[i].acc == YF_INT_MIN) {
+            switch (i) {
+            case YF_GLTF_ATTR_NORM:
+                /* TODO */
+                break;
+            case YF_GLTF_ATTR_TC0:
+                /* TODO */
+                break;
+            case YF_GLTF_ATTR_JNT0:
+                /* TODO */
+                break;
+            case YF_GLTF_ATTR_WGT0:
+                /* TODO */
+                break;
+            default:
+                break;
+            }
+
             continue;
+        }
 
         const T_int comp_type = gltf->accessors.v[attrs[i].acc].comp_type;
         size_t comp_sz;
