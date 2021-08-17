924768e1bfe6c8cc3e3736d2016cb72df6c799a2
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-08-17 19:38:21 -0300

    com test-clock now uses TEST_PRINT() macro

--- yf-com/test/test-clock.c
+++ yf-com/test/test-clock.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 
+#include "test.h"
 #include "yf-clock.h"
 
 /* Tests clock. */
@@ -14,22 +15,24 @@ int yf_test_clock(void)
 {
     double t1, t2;
     const double ts[] = {1.0, 1.5, 0.1, 0.01, 3.125};
+    char s[256] = {0};
 
-    puts("(gettime)");
     t1 = yf_gettime();
-    printf(" %.10f\n\n", t1);
+    snprintf(s, sizeof s, "%.10f", t1);
+    YF_TEST_PRINT("gettime", "", s);
 
-    puts("(gettime)");
     t2 = yf_gettime();
-    printf(" %.10f\n\n", t2);
+    snprintf(s, sizeof s, "%.10f", t2);
+    YF_TEST_PRINT("gettime", "", s);
 
-    printf("elapsed time: %.10fs\n\n", t2 - t1);
+    printf("\n- elapsed time: %.10fs -\n\n", t2 - t1);
 
     for (size_t i = 0; i < (sizeof ts / sizeof *ts); i++) {
-        printf("(sleep %.3f)\n\n", ts[i]);
+        snprintf(s, sizeof s, "%.3f", ts[i]);
+        YF_TEST_PRINT("sleep", s, "");
         t1 = yf_gettime();
         yf_sleep(ts[i]);
-        printf("elapsed time: %.10fs\n\n", yf_gettime() - t1);
+        printf("\n- elapsed time: %.10fs -\n\n", yf_gettime() - t1);
     }
 
     return 0;
