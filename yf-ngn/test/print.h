200b6b6070e1463f17ec54a79cf1a2e9215b4110
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-08-25 15:50:51 -0300

    ngn print_meshdt()/print_texdt() removed

--- yf-ngn/test/print.h
+++ yf-ngn/test/print.h
@@ -10,11 +10,8 @@
 
 #include "node.h"
 #include "mesh.h"
-#include "texture.h"
 
 void yf_print_nodeobj(YF_node node);
 void yf_print_mesh(YF_mesh mesh);
-void yf_print_meshdt(const YF_meshdt *data);
-void yf_print_texdt(const YF_texdt *data);
 
 #endif /* YF_PRINT_H */
