4b7744864c7edca0853215d746c3d330227b043d
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-08-27 21:17:51 -0300

    ngn 'print_tex()' decl. added to 'print.h'

--- yf-ngn/test/print.h
+++ yf-ngn/test/print.h
@@ -10,8 +10,10 @@
 
 #include "node.h"
 #include "mesh.h"
+#include "texture.h"
 
 void yf_print_nodeobj(YF_node node);
 void yf_print_mesh(YF_mesh mesh);
+void yf_print_tex(YF_texture tex);
 
 #endif /* YF_PRINT_H */
