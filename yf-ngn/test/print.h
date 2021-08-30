57edfc1bf888f1fed865651ff24f1aa1fd46584f
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-08-30 12:49:49 -0300

    ngn 'print_coll()' decl. added to 'print.h'

--- yf-ngn/test/print.h
+++ yf-ngn/test/print.h
@@ -12,10 +12,12 @@
 #include "mesh.h"
 #include "texture.h"
 #include "yf-animation.h"
+#include "yf-collection.h"
 
 void yf_print_nodeobj(YF_node node);
 void yf_print_mesh(YF_mesh mesh);
 void yf_print_tex(YF_texture tex);
 void yf_print_anim(YF_animation anim);
+void yf_print_coll(YF_collection coll);
 
 #endif /* YF_PRINT_H */
