bd5a05f972c45807f7dc0f7d30b1d30e9c4df05d
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-08-29 21:36:08 -0300

    ngn 'print_anim()' decl. added to 'print.h'

--- yf-ngn/test/print.h
+++ yf-ngn/test/print.h
@@ -11,9 +11,11 @@
 #include "node.h"
 #include "mesh.h"
 #include "texture.h"
+#include "yf-animation.h"
 
 void yf_print_nodeobj(YF_node node);
 void yf_print_mesh(YF_mesh mesh);
 void yf_print_tex(YF_texture tex);
+void yf_print_anim(YF_animation anim);
 
 #endif /* YF_PRINT_H */
