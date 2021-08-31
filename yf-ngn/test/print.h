f137ed6a9dce90192dac0b3df8711b8fe7484711
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-08-31 19:24:09 -0300

    ngn 'print_cam()' decl. added to 'print.h'

--- yf-ngn/test/print.h
+++ yf-ngn/test/print.h
@@ -11,12 +11,14 @@
 #include "node.h"
 #include "mesh.h"
 #include "texture.h"
+#include "yf-camera.h"
 #include "yf-animation.h"
 #include "yf-collection.h"
 
 void yf_print_nodeobj(YF_node node);
 void yf_print_mesh(YF_mesh mesh);
 void yf_print_tex(YF_texture tex);
+void yf_print_cam(YF_camera cam);
 void yf_print_anim(YF_animation anim);
 void yf_print_coll(YF_collection coll);
 
