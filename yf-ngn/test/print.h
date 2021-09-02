892b41204051a3cddc4e6a5d0c273cbb5d43bfd0
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-09-02 12:28:58 -0300

    ngn 'print_matl()' decl. added to 'print.h'

--- yf-ngn/test/print.h
+++ yf-ngn/test/print.h
@@ -11,6 +11,7 @@
 #include "node.h"
 #include "mesh.h"
 #include "texture.h"
+#include "yf-material.h"
 #include "yf-camera.h"
 #include "yf-animation.h"
 #include "yf-collection.h"
@@ -18,6 +19,7 @@
 void yf_print_nodeobj(YF_node node);
 void yf_print_mesh(YF_mesh mesh);
 void yf_print_tex(YF_texture tex);
+void yf_print_matl(YF_material matl);
 void yf_print_cam(YF_camera cam);
 void yf_print_anim(YF_animation anim);
 void yf_print_coll(YF_collection coll);
