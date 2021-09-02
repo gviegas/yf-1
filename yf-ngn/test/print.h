64935d9ef9198c54f01c5d91b0263ff9fa6a5111
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-09-02 19:03:18 -0300

    ngn print minor

--- yf-ngn/test/print.h
+++ yf-ngn/test/print.h
@@ -8,22 +8,22 @@
 #ifndef YF_PRINT_H
 #define YF_PRINT_H
 
-#include "node.h"
-#include "mesh.h"
-#include "texture.h"
+#include "yf-node.h"
+#include "yf-mesh.h"
+#include "yf-texture.h"
 #include "yf-skin.h"
 #include "yf-material.h"
 #include "yf-camera.h"
 #include "yf-animation.h"
 #include "yf-collection.h"
 
-void yf_print_nodeobj(YF_node node);
-void yf_print_mesh(YF_mesh mesh);
-void yf_print_tex(YF_texture tex);
-void yf_print_skin(YF_skin skin);
-void yf_print_matl(YF_material matl);
-void yf_print_cam(YF_camera cam);
-void yf_print_anim(YF_animation anim);
-void yf_print_coll(YF_collection coll);
+void yf_print_nodeobj(YF_node);
+void yf_print_mesh(YF_mesh);
+void yf_print_tex(YF_texture);
+void yf_print_skin(YF_skin);
+void yf_print_matl(YF_material);
+void yf_print_cam(YF_camera);
+void yf_print_anim(YF_animation);
+void yf_print_coll(YF_collection);
 
 #endif /* YF_PRINT_H */
