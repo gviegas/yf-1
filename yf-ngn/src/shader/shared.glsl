bc445433314ad9ea0c1888c8ef7503e3dc42cb91
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-09-23 19:56:48 -0300

    ngn shader shared data moved to a separate file

---
+++ yf-ngn/src/shader/shared.glsl
@@ -0,0 +1,35 @@
+/*
+ * YF
+ * shared.glsl
+ *
+ * Copyright © 2021 Gustavo C. Viegas.
+ */
+
+#ifndef VPORT_N
+# error "VPORT_N not defined"
+#endif
+
+layout(std140, column_major) uniform;
+
+/**
+ * Type defining a viewport.
+ */
+struct T_vport {
+    float x;
+    float y;
+    float wdt;
+    float hgt;
+    float near;
+    float far;
+};
+
+/**
+ * Global uniform data.
+ */
+layout(set=0, binding=0) uniform U_globl {
+    mat4 v;
+    mat4 p;
+    mat4 o;
+    mat4 vp;
+    T_vport vport[VPORT_N];
+} globl_;
