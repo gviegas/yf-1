bb9b74f6a2d7152ecd8fa5ceaac07678131884ba
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-09-27 19:27:43 -0300

    ngn print_matl() changes

--- yf-ngn/src/material.c
+++ yf-ngn/src/material.c
@@ -72,68 +72,165 @@ void yf_print_matl(YF_material matl)
     switch (matl->prop.pbr) {
     case YF_PBR_SPECGLOSS:
         printf("  pbr: PBR_SPECGLOSS\n"
-               "   diffuse texture: %p\n"
-               "   diffuse factor: [%.4f, %.4f, %.4f, %.4f]\n"
-               "   spec-gloss texture: %p\n"
-               "   specular factor: [%.4f, %.4f, %.4f]\n"
-               "   glossiness factor: %.4f\n",
+               "   diffuse:\n"
+               "    texture: <%p>\n"
+               "    sampler:\n"
+               "     wrap mode (u/v/w): %d/%d/%d\n"
+               "     filter (mag/min/mipmap): %d/%d/%d\n"
+               "    coordinate set: %d\n"
+               "    factor: [%.4f, %.4f, %.4f, %.4f]\n",
                (void *)matl->prop.pbrsg.diffuse_tex.tex,
+               matl->prop.pbrsg.diffuse_tex.splr.wrapmode.u,
+               matl->prop.pbrsg.diffuse_tex.splr.wrapmode.v,
+               matl->prop.pbrsg.diffuse_tex.splr.wrapmode.w,
+               matl->prop.pbrsg.diffuse_tex.splr.filter.mag,
+               matl->prop.pbrsg.diffuse_tex.splr.filter.min,
+               matl->prop.pbrsg.diffuse_tex.splr.filter.mipmap,
+               matl->prop.pbrsg.diffuse_tex.uvset,
                matl->prop.pbrsg.diffuse_fac[0],
                matl->prop.pbrsg.diffuse_fac[1],
                matl->prop.pbrsg.diffuse_fac[2],
-               matl->prop.pbrsg.diffuse_fac[3],
+               matl->prop.pbrsg.diffuse_fac[3]);
+        printf("   spec-gloss:\n"
+               "    texture: <%p>\n"
+               "    sampler:\n"
+               "     wrap mode (u/v/w): %d/%d/%d\n"
+               "     filter (mag/min/mipmap): %d/%d/%d\n"
+               "    coordinate set: %d\n"
+               "    specular factor: [%.4f, %.4f, %.4f]\n"
+               "    glossiness factor: %.4f\n",
                (void *)matl->prop.pbrsg.spec_gloss_tex.tex,
+               matl->prop.pbrsg.spec_gloss_tex.splr.wrapmode.u,
+               matl->prop.pbrsg.spec_gloss_tex.splr.wrapmode.v,
+               matl->prop.pbrsg.spec_gloss_tex.splr.wrapmode.w,
+               matl->prop.pbrsg.spec_gloss_tex.splr.filter.mag,
+               matl->prop.pbrsg.spec_gloss_tex.splr.filter.min,
+               matl->prop.pbrsg.spec_gloss_tex.splr.filter.mipmap,
+               matl->prop.pbrsg.spec_gloss_tex.uvset,
                matl->prop.pbrsg.specular_fac[0],
                matl->prop.pbrsg.specular_fac[1],
                matl->prop.pbrsg.specular_fac[2],
                matl->prop.pbrsg.glossiness_fac);
         break;
+
     case YF_PBR_METALROUGH:
         printf("  pbr: PBR_METALROUGH\n"
-               "   color texture: %p\n"
-               "   color factor: [%.4f, %.4f, %.4f, %.4f]\n"
-               "   metal-rough texture: %p\n"
-               "   metallic factor: %.4f\n"
-               "   roughness factor: %.4f\n",
+               "   color:\n"
+               "    texture: <%p>\n"
+               "    sampler:\n"
+               "     wrap mode (u/v/w): %d/%d/%d\n"
+               "     filter (mag/min/mipmap): %d/%d/%d\n"
+               "    coordinate set: %d\n"
+               "    factor: [%.4f, %.4f, %.4f, %.4f]\n",
                (void *)matl->prop.pbrmr.color_tex.tex,
+               matl->prop.pbrmr.color_tex.splr.wrapmode.u,
+               matl->prop.pbrmr.color_tex.splr.wrapmode.v,
+               matl->prop.pbrmr.color_tex.splr.wrapmode.w,
+               matl->prop.pbrmr.color_tex.splr.filter.mag,
+               matl->prop.pbrmr.color_tex.splr.filter.min,
+               matl->prop.pbrmr.color_tex.splr.filter.mipmap,
+               matl->prop.pbrmr.color_tex.uvset,
                matl->prop.pbrmr.color_fac[0], matl->prop.pbrmr.color_fac[1],
-               matl->prop.pbrmr.color_fac[2], matl->prop.pbrmr.color_fac[3],
+               matl->prop.pbrmr.color_fac[2], matl->prop.pbrmr.color_fac[3]);
+        printf("   metal-rough:\n"
+               "    texture: <%p>\n"
+               "    sampler:\n"
+               "     wrap mode (u/v/w): %d/%d/%d\n"
+               "     filter (mag/min/mipmap): %d/%d/%d\n"
+               "    coordinate set: %d\n"
+               "    metallic factor: %.4f\n"
+               "    roughness factor: %.4f\n",
                (void *)matl->prop.pbrmr.metal_rough_tex.tex,
+               matl->prop.pbrmr.metal_rough_tex.splr.wrapmode.u,
+               matl->prop.pbrmr.metal_rough_tex.splr.wrapmode.v,
+               matl->prop.pbrmr.metal_rough_tex.splr.wrapmode.w,
+               matl->prop.pbrmr.metal_rough_tex.splr.filter.mag,
+               matl->prop.pbrmr.metal_rough_tex.splr.filter.min,
+               matl->prop.pbrmr.metal_rough_tex.splr.filter.mipmap,
+               matl->prop.pbrmr.metal_rough_tex.uvset,
                matl->prop.pbrmr.metallic_fac, matl->prop.pbrmr.roughness_fac);
         break;
+
     case YF_PBR_NONE:
         printf("  pbr: PBR_NONE\n"
-               "   color texture: %p\n"
-               "   color factor: [%.4f, %.4f, %.4f, %.4f]\n",
+               "   color:\n"
+               "    texture: <%p>\n"
+               "    sampler:\n"
+               "     wrap mode (u/v/w): %d/%d/%d\n"
+               "     filter (mag/min/mipmap): %d/%d/%d\n"
+               "    coordinate set: %d\n"
+               "    factor: [%.4f, %.4f, %.4f, %.4f]\n",
                (void *)matl->prop.nopbr.color_tex.tex,
+               matl->prop.nopbr.color_tex.splr.wrapmode.u,
+               matl->prop.nopbr.color_tex.splr.wrapmode.v,
+               matl->prop.nopbr.color_tex.splr.wrapmode.w,
+               matl->prop.nopbr.color_tex.splr.filter.mag,
+               matl->prop.nopbr.color_tex.splr.filter.min,
+               matl->prop.nopbr.color_tex.splr.filter.mipmap,
+               matl->prop.nopbr.color_tex.uvset,
                matl->prop.nopbr.color_fac[0], matl->prop.nopbr.color_fac[1],
                matl->prop.nopbr.color_fac[2], matl->prop.nopbr.color_fac[3]);
         break;
+
     default:
         assert(0);
     }
 
     printf("  normal:\n"
-           "   texture: %p\n"
-           "   scale: %.4f\n"
-           "  occlusion:\n"
-           "   texture: %p\n"
-           "   strength: %.4f\n"
-           "  emissive:\n"
-           "   texture: %p\n"
-           "   factor: [%.4f, %.4f, %.4f]\n"
-           "  alpha mode: ALPHAMODE_",
-           (void*)matl->prop.normal.tex.tex, matl->prop.normal.scale,
-           (void*)matl->prop.occlusion.tex.tex, matl->prop.occlusion.strength,
-           (void*)matl->prop.emissive.tex.tex, matl->prop.emissive.factor[0],
+           "   texture: <%p>\n"
+           "   sampler:\n"
+           "    wrap mode (u/v/w): %d/%d/%d\n"
+           "    filter (mag/min/mipmap): %d/%d/%d\n"
+           "   coordinate set: %d\n"
+           "   scale: %.4f\n",
+           (void*)matl->prop.normal.tex.tex,
+           matl->prop.normal.tex.splr.wrapmode.u,
+           matl->prop.normal.tex.splr.wrapmode.v,
+           matl->prop.normal.tex.splr.wrapmode.w,
+           matl->prop.normal.tex.splr.filter.mag,
+           matl->prop.normal.tex.splr.filter.min,
+           matl->prop.normal.tex.splr.filter.mipmap,
+           matl->prop.normal.tex.uvset, matl->prop.normal.scale);
+
+    printf("  occlusion:\n"
+           "   texture: <%p>\n"
+           "   sampler:\n"
+           "    wrap mode (u/v/w): %d/%d/%d\n"
+           "    filter (mag/min/mipmap): %d/%d/%d\n"
+           "   coordinate set: %d\n"
+           "   strength: %.4f\n",
+           (void*)matl->prop.occlusion.tex.tex,
+           matl->prop.occlusion.tex.splr.wrapmode.u,
+           matl->prop.occlusion.tex.splr.wrapmode.v,
+           matl->prop.occlusion.tex.splr.wrapmode.w,
+           matl->prop.occlusion.tex.splr.filter.mag,
+           matl->prop.occlusion.tex.splr.filter.min,
+           matl->prop.occlusion.tex.splr.filter.mipmap,
+           matl->prop.occlusion.tex.uvset, matl->prop.occlusion.strength);
+
+    printf("  emissive:\n"
+           "   texture: <%p>\n"
+           "   sampler:\n"
+           "    wrap mode (u/v/w): %d/%d/%d\n"
+           "    filter (mag/min/mipmap): %d/%d/%d\n"
+           "   coordinate set: %d\n"
+           "   factor: [%.4f, %.4f, %.4f]\n",
+           (void*)matl->prop.emissive.tex.tex,
+           matl->prop.emissive.tex.splr.wrapmode.u,
+           matl->prop.emissive.tex.splr.wrapmode.v,
+           matl->prop.emissive.tex.splr.wrapmode.w,
+           matl->prop.emissive.tex.splr.filter.mag,
+           matl->prop.emissive.tex.splr.filter.min,
+           matl->prop.emissive.tex.splr.filter.mipmap,
+           matl->prop.emissive.tex.uvset, matl->prop.emissive.factor[0],
            matl->prop.emissive.factor[1], matl->prop.emissive.factor[2]);
 
     switch (matl->prop.alphamode) {
     case YF_ALPHAMODE_OPAQUE:
-        puts("OPAQUE");
+        puts("  alpha mode: ALPHAMODE_OPAQUE");
         break;
     case YF_ALPHAMODE_BLEND:
-        puts("BLEND");
+        puts("  alpha mode: ALPHAMODE_BLEND");
         break;
     default:
         assert(0);
