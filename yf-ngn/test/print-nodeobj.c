25b9b75bf3113adeadb895b912cfe6b46face228
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-09-27 20:30:04 -0300

    ngn print-nodeobj changes

--- yf-ngn/test/print-nodeobj.c
+++ yf-ngn/test/print-nodeobj.c
@@ -26,36 +26,31 @@ void yf_print_nodeobj(YF_node node)
         break;
 
     case YF_NODEOBJ_MODEL: {
-        YF_model mdl = obj;
         YF_skeleton skel = NULL;
-        YF_skin skin = yf_model_getskin(mdl, &skel);
+        YF_skin skin = yf_model_getskin(obj, &skel);
         printf(" NODEOBJ_MODEL <%p>:\n"
-               "  mesh: %p\n"
-               "  skin: %p\n"
-               "  skel: %p\n",
-               (void *)mdl, (void *)yf_model_getmesh(mdl), (void *)skin,
-               (void *)skel);
+               "  mesh: <%p>\n"
+               "  skin: <%p>\n"
+               "  skel: <%p>\n",
+               obj, (void *)yf_model_getmesh(obj), (void *)skin, (void *)skel);
     } break;
 
     case YF_NODEOBJ_TERRAIN: {
-        YF_terrain terr = obj;
         printf(" NODEOBJ_TERRAIN <%p>:\n"
-               "  mesh: %p\n"
-               "  tex:  %p\n"
-               "  hmap: %p\n",
-               (void *)terr, (void *)yf_terrain_getmesh(terr),
-               (void *)yf_terrain_gettex(terr),
-               (void *)yf_terrain_gethmap(terr));
+               "  mesh: <%p>\n"
+               "  tex: <%p>\n"
+               "  hmap: <%p>\n",
+               obj, (void *)yf_terrain_getmesh(obj),
+               (void *)yf_terrain_gettex(obj), (void *)yf_terrain_gethmap(obj));
     } break;
 
     case YF_NODEOBJ_PARTICLE: {
-        YF_particle part = obj;
-        YF_psys *sys = yf_particle_getsys(part);
+        YF_psys *sys = yf_particle_getsys(obj);
         printf(" NODEOBJ_PARTICLE <%p>:\n"
-               "  mesh: %p\n"
-               "  tex:  %p\n",
-               (void *)part, (void *)yf_particle_getmesh(part),
-               (void *)yf_particle_gettex(part));
+               "  mesh: <%p>\n"
+               "  tex: <%p>\n",
+               obj, (void *)yf_particle_getmesh(obj),
+               (void *)yf_particle_gettex(obj));
         printf("  sys:\n"
                "   emitter:\n"
                "    norm: [%.4f, %.4f, %.4f]\n"
@@ -87,49 +82,45 @@ void yf_print_nodeobj(YF_node node)
     } break;
 
     case YF_NODEOBJ_QUAD: {
-        YF_quad quad = obj;
         printf(" NODEOBJ_QUAD <%p>:\n"
-               "  mesh: %p\n"
-               "  tex:  %p\n",
-               (void *)quad, (void *)yf_quad_getmesh(quad),
-               (void *)yf_quad_gettex(quad));
+               "  mesh: <%p>\n"
+               "  tex: <%p>\n",
+               obj, (void *)yf_quad_getmesh(obj), (void *)yf_quad_gettex(obj));
     } break;
 
     case YF_NODEOBJ_LABEL: {
-        YF_label labl = obj;
         size_t n;
-        yf_label_getstr(labl, NULL, &n);
+        yf_label_getstr(obj, NULL, &n);
         wchar_t str[n];
-        yf_label_getstr(labl, str, &n);
-        YF_dim2 dim = yf_label_getdim(labl);
+        yf_label_getstr(obj, str, &n);
+        YF_dim2 dim = yf_label_getdim(obj);
         printf(" NODEOBJ_LABEL <%p>:\n"
-               "  mesh: %p\n"
-               "  tex:  %p\n"
-               "  font: %p\n"
-               "  pt:   %hu\n"
-               "  str:  '%ls' (%zu)\n"
-               "  dim:  %ux%u\n",
-               (void *)labl, (void *)yf_label_getmesh(labl),
-               (void *)yf_label_gettex(labl), (void *)yf_label_getfont(labl),
-               yf_label_getpt(labl), str, n, dim.width, dim.height);
+               "  mesh: <%p>\n"
+               "  tex: <%p>\n"
+               "  font: <%p>\n"
+               "  pt: %hu\n"
+               "  str: '%ls' (%zu)\n"
+               "  dim: %ux%u\n",
+               obj, (void *)yf_label_getmesh(obj),
+               (void *)yf_label_gettex(obj), (void *)yf_label_getfont(obj),
+               yf_label_getpt(obj), str, n, dim.width, dim.height);
     } break;
 
     case YF_NODEOBJ_LIGHT: {
-        YF_light light = obj;
         int lightt;
         YF_vec3 color;
         float intensity, range, inner_angle, outer_angle;
-        yf_light_getval(light, &lightt, color, &intensity, &range,
+        yf_light_getval(obj, &lightt, color, &intensity, &range,
                         &inner_angle, &outer_angle);
         printf(" NODEOBJ_LIGHT <%p>:\n"
-               "  light type:  LIGHTT_%s\n"
-               "  color:       [%.4f, %.4f, %.4f]\n"
-               "  intensity:   %.4f\n"
-               "  range:       %.4f\n"
+               "  light type: LIGHTT_%s\n"
+               "  color: [%.4f, %.4f, %.4f]\n"
+               "  intensity: %.4f\n"
+               "  range: %.4f\n"
                "  inner_angle: %.4f\n"
                "  outer_angle: %.4f\n",
-               (void *)light, (lightt == YF_LIGHTT_POINT ? "POINT" :
-                               (lightt == YF_LIGHTT_SPOT ? "SPOT" : "DIRECT")),
+               obj, (lightt == YF_LIGHTT_POINT ? "POINT" :
+                     (lightt == YF_LIGHTT_SPOT ? "SPOT" : "DIRECT")),
                color[0], color[1], color[2], intensity, range, inner_angle,
                outer_angle);
     } break;
