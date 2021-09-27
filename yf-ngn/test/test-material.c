d0b7773a5fe54bc37a2eff56ef736b05746059b2
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-09-26 21:09:33 -0300

    ngn test-material corrected for 'texref'

--- yf-ngn/test/test-material.c
+++ yf-ngn/test/test-material.c
@@ -11,37 +11,48 @@
 #include "print.h"
 #include "yf-material.h"
 
+#define YF_CMPTEXREF(a, b) do { \
+    if ((a).tex != (b).tex || \
+        (a).splr.wrapmode.u != (b).splr.wrapmode.u || \
+        (a).splr.wrapmode.v != (b).splr.wrapmode.v || \
+        (a).splr.wrapmode.w != (b).splr.wrapmode.w || \
+        (a).splr.filter.mag != (b).splr.filter.mag || \
+        (a).splr.filter.min != (b).splr.filter.min || \
+        (a).splr.filter.mipmap != (b).splr.filter.mipmap || \
+        (a).uvset != (b).uvset) \
+        return -1; } while (0)
+
 #define YF_CMPMATL(a, b) do { \
     if ((a)->pbr != (b)->pbr) \
         return -1; \
     if ((a)->pbr == YF_PBR_SPECGLOSS) { \
-        if ((a)->pbrsg.diffuse_tex != (b)->pbrsg.diffuse_tex || \
-            !yf_vec4_iseq((a)->pbrsg.diffuse_fac, (b)->pbrsg.diffuse_fac) || \
-            (a)->pbrsg.spec_gloss_tex != (b)->pbrsg.spec_gloss_tex || \
+        YF_CMPTEXREF((a)->pbrsg.diffuse_tex, (b)->pbrsg.diffuse_tex); \
+        YF_CMPTEXREF((a)->pbrsg.spec_gloss_tex, (b)->pbrsg.spec_gloss_tex); \
+        if (!yf_vec4_iseq((a)->pbrsg.diffuse_fac, (b)->pbrsg.diffuse_fac) || \
             !yf_vec3_iseq((a)->pbrsg.specular_fac, (b)->pbrsg.specular_fac) || \
             (a)->pbrsg.glossiness_fac != (b)->pbrsg.glossiness_fac) \
             return -1; \
     } else if ((a)->pbr == YF_PBR_METALROUGH) { \
-        if ((a)->pbrmr.color_tex != (b)->pbrmr.color_tex || \
-            !yf_vec4_iseq((a)->pbrmr.color_fac, (b)->pbrmr.color_fac) || \
-            (a)->pbrmr.metal_rough_tex != (b)->pbrmr.metal_rough_tex || \
+        YF_CMPTEXREF((a)->pbrmr.color_tex, (b)->pbrmr.color_tex); \
+        YF_CMPTEXREF((a)->pbrmr.metal_rough_tex, (b)->pbrmr.metal_rough_tex); \
+        if (!yf_vec4_iseq((a)->pbrmr.color_fac, (b)->pbrmr.color_fac) || \
             (a)->pbrmr.metallic_fac != (b)->pbrmr.metallic_fac || \
             (a)->pbrmr.roughness_fac != (b)->pbrmr.roughness_fac) \
             return -1; \
     } else if ((a)->pbr == YF_PBR_NONE) { \
-        if ((a)->nopbr.color_tex != (b)->nopbr.color_tex || \
-            !yf_vec4_iseq((a)->nopbr.color_fac, (b)->nopbr.color_fac)) \
+        YF_CMPTEXREF((a)->nopbr.color_tex, (b)->nopbr.color_tex); \
+        if (!yf_vec4_iseq((a)->nopbr.color_fac, (b)->nopbr.color_fac)) \
             return -1; \
     } else \
         return -1; \
-    if ((a)->normal.tex != (b)->normal.tex || \
-        (a)->normal.scale != (b)->normal.scale) \
+    YF_CMPTEXREF((a)->normal.tex, (b)->normal.tex); \
+    if ((a)->normal.scale != (b)->normal.scale) \
         return -1; \
-    if ((a)->occlusion.tex != (b)->occlusion.tex || \
-        (a)->occlusion.strength != (b)->occlusion.strength) \
+    YF_CMPTEXREF((a)->occlusion.tex, (b)->occlusion.tex); \
+    if ((a)->occlusion.strength != (b)->occlusion.strength) \
         return -1; \
-    if ((a)->emissive.tex != (b)->emissive.tex || \
-        !yf_vec3_iseq((a)->emissive.factor, (b)->emissive.factor)) \
+    YF_CMPTEXREF((a)->emissive.tex, (b)->emissive.tex); \
+    if (!yf_vec3_iseq((a)->emissive.factor, (b)->emissive.factor)) \
         return -1; \
     if ((a)->alphamode != (b)->alphamode) \
         return -1; } while (0)
@@ -59,22 +70,22 @@ int yf_test_material(void)
     YF_matlprop prop_sg = {
         .pbr = YF_PBR_SPECGLOSS,
         .pbrsg = {
-            .diffuse_tex = NULL,
+            .diffuse_tex = {0},
             .diffuse_fac = {1.0f, 0.5f, 0.25f, 0.125f},
-            .spec_gloss_tex = NULL,
+            .spec_gloss_tex = {0},
             .specular_fac = {0.667f, 0.333f, 0.0f},
             .glossiness_fac = 0.999f
         },
         .normal = {
-            .tex = NULL,
+            .tex = {0},
             .scale = 0.25f
         },
         .occlusion = {
-            .tex = NULL,
+            .tex = {0},
             .strength = 0.45f
         },
         .emissive = {
-            .tex = NULL,
+            .tex = {0},
             .factor = {0.1f, 0.01f, 0.001f}
         },
         .alphamode = YF_ALPHAMODE_BLEND
@@ -90,22 +101,22 @@ int yf_test_material(void)
     YF_matlprop prop_mr = {
         .pbr = YF_PBR_METALROUGH,
         .pbrmr = {
-            .color_tex = NULL,
+            .color_tex = {0},
             .color_fac = {0.2f, 0.4f, 0.8f, 1.0f},
-            .metal_rough_tex = NULL,
+            .metal_rough_tex = {0},
             .metallic_fac = 0.5f,
             .roughness_fac = 0.2f
         },
         .normal = {
-            .tex = NULL,
+            .tex = {0},
             .scale = 0.625f
         },
         .occlusion = {
-            .tex = NULL,
+            .tex = {0},
             .strength = 0.175f
         },
         .emissive = {
-            .tex = NULL,
+            .tex = {0},
             .factor = {0.3f, 0.2f, 0.1f}
         },
         .alphamode = YF_ALPHAMODE_OPAQUE
@@ -121,12 +132,12 @@ int yf_test_material(void)
     YF_matlprop prop_ul = {
         .pbr = YF_PBR_NONE,
         .nopbr = {
-            .color_tex = NULL,
+            .color_tex = {0},
             .color_fac = {0.01f, 0.02f, 0.04f, 1.0f}
         },
-        .normal = {0},
-        .occlusion = {0},
-        .emissive = {0},
+        .normal = {{0}},
+        .occlusion = {{0}},
+        .emissive = {{0}},
         .alphamode = YF_ALPHAMODE_OPAQUE
     };
 
