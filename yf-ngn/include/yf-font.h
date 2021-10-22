ba666cb07d639e8146ab45013b57805d6b6d0e10
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-10-22 10:50:01 -0300

    ngn font 'glyph' and 'fontdt' types moved to public header

--- yf-ngn/include/yf-font.h
+++ yf-ngn/include/yf-font.h
@@ -9,6 +9,7 @@
 #define YF_YF_FONT_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 #include "yf/com/yf-defs.h"
 
@@ -31,6 +32,47 @@ typedef struct YF_font_o *YF_font;
  *  the global error is set to indicate the cause.
  */
 YF_font yf_font_load(const char *pathname, size_t index, YF_collection coll);
+
+/**
+ * Type defining a font glyph.
+ */
+typedef struct {
+    uint16_t width;
+    uint16_t height;
+
+    /* Greyscale format, either 8 or 16 bits per pixel. */
+    uint16_t bpp;
+    union {
+        uint8_t *u8;
+        uint16_t *u16;
+    } bitmap;
+
+    /* Horizontal metrics. */
+    int16_t base_h;
+    uint16_t adv_wdt;
+    int16_t lsb;
+} YF_glyph;
+
+/**
+ * Type defining font data.
+ */
+typedef struct {
+    /* Font implementation. */
+    void *font;
+
+    /* Glyph generation. */
+    int (*glyph)(void *font, wchar_t code, uint16_t pt, uint16_t dpi,
+                 YF_glyph *glyph);
+
+    /* General metrics, scaled to given 'pt' and 'dpi' values. */
+    /* TODO: Other metrics besides bbox. */
+    void (*metrics)(void *font, uint16_t pt, uint16_t dpi,
+                    int16_t *x_min, int16_t *y_min,
+                    int16_t *x_max, int16_t *y_max);
+
+    /* Deinitialization. */
+    void (*deinit)(void *font);
+} YF_fontdt;
 
 /**
  * Deinitializes a font.
--- yf-ngn/src/font.h
+++ yf-ngn/src/font.h
@@ -8,49 +8,10 @@
 #ifndef YF_FONT_H
 #define YF_FONT_H
 
-#include <stdint.h>
-
 #include "yf/com/yf-types.h"
 
 #include "yf-font.h"
 #include "yf-texture.h"
-
-/* Font glyph. */
-typedef struct {
-    uint16_t width;
-    uint16_t height;
-
-    /* Greyscale format, either 8 or 16 bits per pixel. */
-    uint16_t bpp;
-    union {
-        uint8_t *u8;
-        uint16_t *u16;
-    } bitmap;
-
-    /* Horizontal metrics. */
-    int16_t base_h;
-    uint16_t adv_wdt;
-    int16_t lsb;
-} YF_glyph;
-
-/* Font data. */
-typedef struct {
-    /* Font implementation. */
-    void *font;
-
-    /* Glyph generation. */
-    int (*glyph)(void *font, wchar_t code, uint16_t pt, uint16_t dpi,
-                 YF_glyph *glyph);
-
-    /* General metrics, scaled to given 'pt' and 'dpi' values. */
-    /* TODO: Other metrics besides bbox. */
-    void (*metrics)(void *font, uint16_t pt, uint16_t dpi,
-                    int16_t *x_min, int16_t *y_min,
-                    int16_t *x_max, int16_t *y_max);
-
-    /* Deinitialization. */
-    void (*deinit)(void *font);
-} YF_fontdt;
 
 /* Initializes a new font object from font data directly. */
 YF_font yf_font_initdt(const YF_fontdt *data);
