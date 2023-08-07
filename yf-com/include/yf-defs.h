856e616644b84c780f380b0600b14a695729e17f
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-08-07 11:05:16 -0300

    com add visibility definitions

--- yf-com/include/yf-defs.h
+++ yf-com/include/yf-defs.h
@@ -11,6 +11,7 @@
 /**
  * Linkage macros.
  */
+
 #ifdef __cplusplus
 # define YF_DECLS_BEGIN extern "C" {
 # define YF_DECLS_END }
@@ -22,11 +23,32 @@
 /**
  * Compilation attributes.
  */
+
 #ifndef YF_UNUSED
 # ifdef __GNUC__
 #  define YF_UNUSED __attribute__ ((unused))
 # else
 #  define YF_UNUSED
+# endif
+#endif
+
+#ifndef YF_API
+# ifdef _WIN32
+#  error "TODO"
+# elif defined(__GNUC__) && __GNUC__ >= 4
+#  define YF_API __attribute__ ((visibility("default")))
+# else
+#  define YF_API
+# endif
+#endif
+
+#ifndef YF_HIDDEN
+# ifdef _WIN32
+#  error "TODO"
+# elif defined(__GNUC__) && __GNUC__ >= 4
+#  define YF_HIDDEN __attribute__ ((visibility("hidden")))
+# else
+#  define YF_HIDDEN
 # endif
 #endif
 
